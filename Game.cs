5767e33c984c1dc21bfcf958ffc5fa7b5ae3893a
FM <108034450+fmsintez@users.noreply.github.com>
2023-06-27 21:10:33 +0200

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,23 @@
+﻿using Services.Input;
+using UnityEngine;
+
+namespace Infrastructure
+{
+    public class Game
+    {
+        public static IInputServices InputServices;
+
+        public Game()
+        {
+            RegisterInputService();
+        } 
+
+        private static void RegisterInputService()
+        {
+            if (Application.isEditor)
+                InputServices = new StandaloneInputService();
+            else
+                InputServices = new MobileInputService();
+        }
+    }
+}
